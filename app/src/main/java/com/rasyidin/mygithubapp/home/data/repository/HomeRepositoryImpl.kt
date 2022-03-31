abf97e956572cc3dfb3818569235d8113dea95f8
RafliRasyidin <raflirasyidin20@gmail.com>
2022-03-31 21:12:42 +0700

    refactor code

--- app/src/main/java/com/rasyidin/mygithubapp/home/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/rasyidin/mygithubapp/home/data/repository/HomeRepositoryImpl.kt
@@ -3,7 +3,7 @@ package com.rasyidin.mygithubapp.home.data.repository
 import com.rasyidin.mygithubapp.core.domain.ResultState
 import com.rasyidin.mygithubapp.core.utils.fetch
 import com.rasyidin.mygithubapp.core.utils.mapResult
-import com.rasyidin.mygithubapp.home.data.source.remote.HomeRemoteDataSource
+import com.rasyidin.mygithubapp.home.data.source.remote.network.HomeService
 import com.rasyidin.mygithubapp.home.domain.model.Event
 import com.rasyidin.mygithubapp.home.domain.repository.HomeRepository
 import com.rasyidin.mygithubapp.home.domain.utils.toEvent
@@ -11,19 +11,19 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
-class HomeRepositoryImpl @Inject constructor(private val homeRemoteDataSource: HomeRemoteDataSource) :
+class HomeRepositoryImpl @Inject constructor(private val apiService: HomeService) :
     HomeRepository {
 
     override suspend fun getEvents(username: String): Flow<ResultState<List<Event>>> {
         return fetch {
-            homeRemoteDataSource.getEvents(username)
+            apiService.getEvents(username)
         }.map { result ->
             mapResult(result) {
                 this?.map { eventResponseItem ->
-                    val event = eventResponseItem
-                    val repoName = event.repoResponse?.name?.substringAfter("/").toString()
-                    val name = event.repoResponse?.name?.substringBefore("/").toString()
-                    eventResponseItem.repoResponse = homeRemoteDataSource.getRepository(name, repoName)
+                    val repoResponse = eventResponseItem.repoResponse
+                    val repoName = repoResponse?.name?.substringAfter("/").toString()
+                    val name = repoResponse?.name?.substringBefore("/").toString()
+                    eventResponseItem.repoResponse = apiService.getRepository(name, repoName)
                     eventResponseItem.toEvent()
                 }
             }
