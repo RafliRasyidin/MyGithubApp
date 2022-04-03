3643747c7389040c2d9e810145382a732ac44567
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-03 11:53:35 +0700

    setup profile pager adapter

---
+++ app/src/main/java/com/rasyidin/mygithubapp/ui/component/ProfilePagerAdapter.kt
@@ -0,0 +1,28 @@
+package com.rasyidin.mygithubapp.ui.component
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.rasyidin.mygithubapp.R
+import com.rasyidin.mygithubapp.profile.presentation.FollowersFollowingFragment
+import com.rasyidin.mygithubapp.profile.presentation.RepositoryFragment
+
+class ProfilePagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return TAB_TITLES.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 ->RepositoryFragment()
+            else -> FollowersFollowingFragment.newInstance(position)
+        }
+    }
+
+    companion object {
+        val TAB_TITLES = intArrayOf(R.string.repositories, R.string.followers, R.string.following)
+    }
+}
