43cd2db835f65d9605259f48d2084a06df0cd2bc
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-03 11:53:24 +0700

    observe data profile

--- app/src/main/java/com/rasyidin/mygithubapp/profile/presentation/ProfileFragment.kt
+++ app/src/main/java/com/rasyidin/mygithubapp/profile/presentation/ProfileFragment.kt
@@ -1,7 +1,133 @@
 package com.rasyidin.mygithubapp.profile.presentation
 
-import androidx.fragment.app.Fragment
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import com.bumptech.glide.Glide
+import com.google.android.material.tabs.TabLayoutMediator
+import com.rasyidin.mygithubapp.R
+import com.rasyidin.mygithubapp.core.utils.onSuccess
+import com.rasyidin.mygithubapp.databinding.FragmentProfileBinding
+import com.rasyidin.mygithubapp.ui.component.FragmentBinding
+import com.rasyidin.mygithubapp.ui.component.ProfilePagerAdapter
+import com.rasyidin.mygithubapp.ui.component.ProfilePagerAdapter.Companion.TAB_TITLES
+import com.rasyidin.mygithubapp.ui.helper.isLoading
+import com.rasyidin.mygithubapp.ui.helper.isSuccess
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import javax.inject.Inject
 
-class ProfileFragment : Fragment() {
+@AndroidEntryPoint
+class ProfileFragment : FragmentBinding<FragmentProfileBinding>(FragmentProfileBinding::inflate) {
+
+    @Inject
+    lateinit var profileViewModelFactory: ProfileViewModel.AssistedFactory
+
+    private val viewModel: ProfileViewModel by activityViewModels {
+        ProfileViewModel.provideFactory(profileViewModelFactory, "RafliRasyidin")
+    }
+
+    private lateinit var mediator: TabLayoutMediator
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupViewPager()
+
+        setupAdapter()
+
+        observeProfile()
+
+    }
+
+    private fun observeProfile() {
+        lifecycleScope.launchWhenCreated {
+            viewModel.user.collect { result ->
+
+                binding.shimmerAppBar.isVisible = isLoading(result)
+
+                binding.appBar.isVisible = isSuccess(result)
+
+                binding.containerTab.isVisible = isSuccess(result)
+
+                result.onSuccess { user ->
+                    val isBioEmpty = user?.bio.isNullOrEmpty()
+                    val isEmailEmpty = user?.email.isNullOrEmpty()
+                    val isCompanyEmpty = user?.company.isNullOrEmpty()
+                    val isLocationEmpty = user?.location.isNullOrEmpty()
+
+                    binding.apply {
+                        if (isBioEmpty) {
+                            tvBio.visibility = View.GONE
+                        } else {
+                            tvBio.visibility = View.VISIBLE
+                        }
+
+                        if (isEmailEmpty) {
+                            imgEmail.visibility = View.GONE
+                        } else {
+                            imgEmail.visibility = View.VISIBLE
+                        }
+
+                        if (isCompanyEmpty) {
+                            imgCompany.visibility = View.GONE
+                        } else {
+                            imgCompany.visibility = View.VISIBLE
+                        }
+
+                        if (isLocationEmpty) {
+                            imgLocation.visibility = View.GONE
+                        } else {
+                            imgLocation.visibility = View.VISIBLE
+                        }
+
+                        Glide.with(requireActivity())
+                            .load(user?.avatarUrl)
+                            .placeholder(R.drawable.ic_profile_placeholder)
+                            .error(R.drawable.ic_profile_placeholder)
+                            .into(imgUser)
+
+                        tvToolbar.text = user?.username
+                        tvBio.text = user?.bio
+                        tvCompany.text = user?.company
+                        tvEmail.text = user?.email
+                        tvFollowersCount.text = user?.followers.toString()
+                        tvFollowingCount.text = user?.following.toString()
+                        tvRepoCount.text = user?.publicRepos.toString()
+                        tvLocation.text = user?.location
+                        tvName.text = user?.name
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun setupViewPager() {
+        val pagerAdapter by lazy {
+            ProfilePagerAdapter(childFragmentManager, lifecycle)
+        }
+        binding.vp.adapter = pagerAdapter
+    }
+
+    private fun setupAdapter() {
+        mediator = TabLayoutMediator(binding.tabs, binding.vp) { tabs, pos ->
+            tabs.text = when (pos) {
+                0 -> getString(TAB_TITLES[0])
+                1 -> getString(TAB_TITLES[1])
+                else -> getString(TAB_TITLES[2])
+            }
+        }
+        mediator.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mediator.detach()
+        binding.vp.adapter = null
+        _binding = null
+    }
 
 }
