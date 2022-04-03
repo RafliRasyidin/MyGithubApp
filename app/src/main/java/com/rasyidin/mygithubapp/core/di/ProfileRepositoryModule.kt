906f22af6f486ead2caeefa119f762e706cb7702
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-03 11:54:55 +0700

    refactor code

--- app/src/main/java/com/rasyidin/mygithubapp/core/di/ProfileRepositoryModule.kt
+++ app/src/main/java/com/rasyidin/mygithubapp/core/di/ProfileRepositoryModule.kt
@@ -1,8 +1,6 @@
 package com.rasyidin.mygithubapp.core.di
 
 import com.rasyidin.mygithubapp.profile.data.repository.ProfileRepositoryImpl
-import com.rasyidin.mygithubapp.profile.data.source.remote.ProfileRemoteDataSource
-import com.rasyidin.mygithubapp.profile.data.source.remote.ProfileRemoteDataSourceImpl
 import com.rasyidin.mygithubapp.profile.data.source.remote.network.ProfileService
 import com.rasyidin.mygithubapp.profile.domain.repository.ProfileRepository
 import dagger.Module
@@ -17,13 +15,7 @@ class ProfileRepositoryModule {
 
     @Provides
     @Singleton
-    fun provideRemoteDataSource(apiService: ProfileService): ProfileRemoteDataSource {
-        return ProfileRemoteDataSourceImpl(apiService)
-    }
-
-    @Provides
-    @Singleton
-    fun provideRepository(profileRemoteDataSource: ProfileRemoteDataSource): ProfileRepository {
-        return ProfileRepositoryImpl(profileRemoteDataSource)
+    fun provideRepository(apiService: ProfileService): ProfileRepository {
+        return ProfileRepositoryImpl(apiService)
     }
 }
