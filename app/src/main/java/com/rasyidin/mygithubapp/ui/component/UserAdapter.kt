d2f8ec3070bca2d125141a5f283ee341cb90cfaf
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-03 12:06:37 +0700

    create user adapter

---
+++ app/src/main/java/com/rasyidin/mygithubapp/ui/component/UserAdapter.kt
@@ -0,0 +1,51 @@
+package com.rasyidin.mygithubapp.ui.component
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.rasyidin.mygithubapp.R
+import com.rasyidin.mygithubapp.databinding.ItemUserBinding
+import com.rasyidin.mygithubapp.profile.domain.model.User
+
+class UserAdapter: ListAdapter<User, UserAdapter.UserViewHolder>(DiffCallback) {
+
+    var onItemClick: ((User) -> Unit)? = null
+
+    class UserViewHolder(val binding: ItemUserBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(user: User) {
+            binding.apply {
+                Glide.with(imgUser)
+                    .load(user.avatarUrl)
+                    .placeholder(R.drawable.ic_profile_placeholder)
+                    .error(R.drawable.ic_profile_placeholder)
+                    .into(imgUser)
+
+                tvBio.text = user.type
+                tvUsername.text = user.username
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val binding = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return UserViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = getItem(position)
+        holder.bind(user)
+    }
+
+    private object DiffCallback: DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem.username == newItem.username
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+    }
+}
