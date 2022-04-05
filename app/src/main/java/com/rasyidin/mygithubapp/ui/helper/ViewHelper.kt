e4e64735c6bb2c251e70e9c0c63f4100ff998936
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-05 23:44:13 +0700

    add String ext function

--- app/src/main/java/com/rasyidin/mygithubapp/ui/helper/ViewHelper.kt
+++ app/src/main/java/com/rasyidin/mygithubapp/ui/helper/ViewHelper.kt
@@ -1,6 +1,10 @@
 package com.rasyidin.mygithubapp.ui.helper
 
 import com.rasyidin.mygithubapp.core.domain.ResultState
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import kotlin.math.ln
+import kotlin.math.pow
 
 fun <T: Any> isLoading(result: ResultState<T>): Boolean {
     return result is ResultState.Loading
@@ -12,4 +16,18 @@ fun <T: Any> isSuccess(result: ResultState<T>): Boolean {
 
 fun <T: Any> isFailure(result: ResultState<T>): Boolean {
     return result is ResultState.Error
+}
+
+fun String.toShortNumber(): String {
+    val doubleNumber = this.toDouble()
+    val formatter = DecimalFormat("0.#")
+    val isNumberLessThanThousand = this.toInt() < 1000.0
+    formatter.roundingMode = RoundingMode.DOWN
+
+    return if (isNumberLessThanThousand) {
+        formatter.format(doubleNumber)
+    } else {
+        val exp = (ln(doubleNumber) / ln(1000.0)).toInt()
+        formatter.format(this.toInt() / 1000.0.pow(exp.toDouble())) + "KMBTPE"[exp - 1]
+    }
 }
