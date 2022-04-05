06dfc491417cb55fbbb3dea01da8cc3bba08c715
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-05 23:40:27 +0700

    add item click event and showing bottom navigation bar

--- app/src/main/java/com/rasyidin/mygithubapp/home/presentation/HomeFragment.kt
+++ app/src/main/java/com/rasyidin/mygithubapp/home/presentation/HomeFragment.kt
@@ -1,13 +1,19 @@
 package com.rasyidin.mygithubapp.home.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rasyidin.mygithubapp.R
 import com.rasyidin.mygithubapp.core.utils.onFailure
 import com.rasyidin.mygithubapp.core.utils.onSuccess
 import com.rasyidin.mygithubapp.databinding.FragmentHomeBinding
@@ -16,7 +22,6 @@ import com.rasyidin.mygithubapp.ui.component.FragmentBinding
 import com.rasyidin.mygithubapp.ui.helper.isLoading
 import com.rasyidin.mygithubapp.ui.helper.isSuccess
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
 
 @AndroidEntryPoint
 class HomeFragment : FragmentBinding<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
@@ -32,6 +37,36 @@ class HomeFragment : FragmentBinding<FragmentHomeBinding>(FragmentHomeBinding::i
 
         observeEvents()
 
+        onItemListClick()
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showBotNavigation()
+    }
+
+    private fun showBotNavigation() {
+        val botNav = (activity as AppCompatActivity).findViewById<BottomNavigationView>(R.id.bot_nav_view)
+        botNav.visibility = View.VISIBLE
+    }
+
+    private fun onItemListClick() {
+        eventAdapter.onProfileClick = { username ->
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToDetailProfileFragment(username))
+        }
+
+        eventAdapter.onRepoClick = { repository ->
+            val uri = repository?.htmlUrl?.toUri()
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
+
+        eventAdapter.onReleaseClick = { repository ->
+            val uri = repository?.htmlUrl?.toUri()
+            val intent = Intent(Intent.ACTION_VIEW, uri)
+            startActivity(intent)
+        }
     }
 
     private fun observeEvents() {
