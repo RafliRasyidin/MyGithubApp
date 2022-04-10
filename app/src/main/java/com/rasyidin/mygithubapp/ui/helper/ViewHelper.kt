b5ca65a5353ed0590e90cbb926888926c93f291e
RafliRasyidin <raflirasyidin20@gmail.com>
2022-04-10 19:55:15 +0700

    fix null exception

--- app/src/main/java/com/rasyidin/mygithubapp/ui/helper/ViewHelper.kt
+++ app/src/main/java/com/rasyidin/mygithubapp/ui/helper/ViewHelper.kt
@@ -18,16 +18,20 @@ fun <T: Any> isFailure(result: ResultState<T>): Boolean {
     return result is ResultState.Error
 }
 
-fun String.toShortNumber(): String {
-    val doubleNumber = this.toDouble()
-    val formatter = DecimalFormat("0.#")
-    val isNumberLessThanThousand = this.toInt() < 1000.0
-    formatter.roundingMode = RoundingMode.DOWN
+fun String.toShortNumber(): String? {
+    return try {
+        val doubleNumber = this.toDouble()
+        val formatter = DecimalFormat("0.#")
+        val isNumberLessThanThousand = this.toInt() < 1000.0
+        formatter.roundingMode = RoundingMode.DOWN
 
-    return if (isNumberLessThanThousand) {
-        formatter.format(doubleNumber)
-    } else {
-        val exp = (ln(doubleNumber) / ln(1000.0)).toInt()
-        formatter.format(this.toInt() / 1000.0.pow(exp.toDouble())) + "KMBTPE"[exp - 1]
+        if (isNumberLessThanThousand) {
+            formatter.format(doubleNumber)
+        } else {
+            val exp = (ln(doubleNumber) / ln(1000.0)).toInt()
+            formatter.format(this.toInt() / 1000.0.pow(exp.toDouble())) + "KMBTPE"[exp - 1]
+        }
+    } catch (e: Exception) {
+        "0"
     }
 }
